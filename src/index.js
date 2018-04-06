db60c969e8e8edfa34dc38d5d23a69d9d780d741
Ronan Dineen <ronan@ematix19.ematix>
2018-04-06 15:35:13 +0200

    storepicker

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/Storepicker";
+
+import "./css/style.css";
+
+render(<StorePicker />, document.querySelector("#main"));
